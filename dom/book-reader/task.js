664a808cbb5932935a31edb6c1c9ec6dad8c9011
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-07-03 13:38:26 +1100

    Update task.js

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,19 +1,34 @@
 const bookElement = document.getElementById("book");
 const controlElements = document.querySelector(".book__controls");
 
+let carrentClassName = 'book';
 let carrentData = {
 	'book__control_font-size': {'key': undefined, 'dataset': 'size', 'className': 'book_fs-'},
 	'book__control_color': {'key': undefined, 'dataset': 'textColor', 'className': 'book_color-'},
 	'book__control_background': {'key': undefined, 'dataset': 'bgColor', 'className': 'book_bg-'},
 	}
+
+function getCarrentClassName(event) {
 	
-controlElements.addEventListener("click", function (event) {
+	let carrentClassName = 'book';
 	
-	event.preventDefault();
 	activeElement = event.target;
 	parentElement = activeElement.parentElement;
 	
 	carrentData[parentElement.classList[1]]['key'] = activeElement.dataset[carrentData[parentElement.classList[1]]['dataset']];
+	
+	for (const key in carrentData) {
+		if (carrentData[key]['key'] !== undefined) {
+			carrentClassName = carrentClassName + ' ' + carrentData[key]['className'] + carrentData[key]['key'];
+		}
+	}
+	
+	return carrentClassName;	
+}
+	
+controlElements.addEventListener("click", function (event) {
+	
+	event.preventDefault();
 	
 	activeList = parentElement.querySelectorAll("a");
 	
@@ -22,12 +37,16 @@ controlElements.addEventListener("click", function (event) {
 		 });
 	activeElement.classList.add(activeElement.classList[0] + '_active');
 	
-	bookElement.className = 'book';
+	carrentClassName = getCarrentClassName(event);
+	bookElement.className = carrentClassName;
 	
-	for (const key in carrentData) {
-		if (carrentData[key]['key'] !== undefined) {
-			bookElement.className = bookElement.className + ' ' + carrentData[key]['className'] + carrentData[key]['key'];
-		}
-	}
 }
 );
+
+controlElements.addEventListener("mouseover", function (event) {
+	
+	bookElement.className = getCarrentClassName(event);
+}
+);
+
+controlElements.addEventListener("mouseout", () => {bookElement.className = carrentClassName});
