dab80ae979f297ec7c52e0211695fc84416756bb
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-07-31 12:57:22 +1100

    Update task.js

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,33 +1,38 @@
 const signIn = document.getElementById('signin');
+const formName = document.getElementById('signin__form');
+const submitBtn = document.getElementById('signin__btn');
 
-if (localStorage.user_id) {
+const intervalId = setInterval(function() {
+  localStorage.removeItem('user_id');
+}, 5000)
+
+signIn.classList.add('signin_active');
+if (localStorage.getItem('user_id')) {
     document.getElementById('user_id').textContent = localStorage.user_id;
     document.getElementById('welcome').classList.add('welcome_active');
-} 
-else {
-    signIn.classList.add('signin_active');
-    const formName = document.getElementById('signin__form');
-    const submitBtn = document.getElementById('signin__btn');
-
-    submitBtn.addEventListener('click', () => {
-        event.preventDefault();
-        const formData = new FormData(formName);
-        const request = new XMLHttpRequest();
-        request.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-        request.addEventListener('readystatechange', function() {
-        if (this.readyState == 4 && this.status >= 200 && this.status < 300) {
-            const data = JSON.parse(this.responseText); 
-            if (data.success === true) {    
-                localStorage.setItem('user_id', data.user_id);
-                document.getElementById('user_id').textContent = data.user_id;
-                signIn.classList.remove('signin_active');
-                document.getElementById('welcome').classList.add('welcome_active');
-            } 
-            else {
-                alert('Неверный логин/пароль');
-            }
-        };
-        });
-        request.send(formData);
-    });
+	signIn.classList.remove('signin_active');
 }
+
+submitBtn.addEventListener('click', () => {
+	event.preventDefault();
+	const formData = new FormData(formName);
+	const request = new XMLHttpRequest();
+	request.responseType = 'json';
+	request.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+	request.addEventListener('load', function() {
+        if (this.status >= 200 && this.status < 300) {
+            const data = this.response; 
+			if (data.success === true) {    
+				localStorage.setItem('user_id', data.user_id);
+				document.getElementById('user_id').textContent = data.user_id;
+				signIn.classList.remove('signin_active');
+				document.getElementById('welcome').classList.add('welcome_active');
+			} 
+			else {
+				alert('Неверный логин/пароль');
+			}
+        };
+	});
+	request.send(formData);
+	formName.reset();
+});
